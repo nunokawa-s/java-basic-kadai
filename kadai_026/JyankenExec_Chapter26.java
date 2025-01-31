43d6bf6b1c7a107c16533e831c3af5b06842b30c
nunokawa-s <momenn54@gmail.com>
2025-01-31 12:35:19 +0900

    Add files via upload

---
+++ kadai_026/JyankenExec_Chapter26.java
@@ -0,0 +1,13 @@
+package kadai_026;
+
+public class JyankenExec_Chapter26 {
+
+	public static void main(String[] args) {
+		// ゲームオブジェクトを作成
+		Jyanken_Chapter26 game = new Jyanken_Chapter26();
+
+		// ゲームを実行
+		game.playGame();
+
+	}
+}
